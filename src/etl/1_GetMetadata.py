0572708b503edf95f8304df2f92473b5a99c8cba
Suraj Kumar <surajk732000@gmail.com>
2020-05-24 00:07:12 +0530

    test set processing

--- src/etl/1_GetMetadata.py
+++ src/etl/1_GetMetadata.py
@@ -17,24 +17,24 @@ with open(os.path.join(WDIR, "../../SETTINGS.json")) as f:
 # SCRIPT #
 ##########
 
-train_dicoms_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TRAIN_DICOMS_DIR"])
+# train_dicoms_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TRAIN_DICOMS_DIR"])
 test_dicoms_dir  = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TEST_DICOMS_DIR"])
 
-list_of_train_dicoms = glob.glob(os.path.join(train_dicoms_dir, "*"))
+# list_of_train_dicoms = glob.glob(os.path.join(train_dicoms_dir, "*"))
 list_of_test_dicoms  = glob.glob(os.path.join(test_dicoms_dir, "*"))
 
-train_metadata = pd.DataFrame() 
-for i, each_train_dicom in enumerate(list_of_train_dicoms): 
-    sys.stdout.write("Getting metadata: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
-    sys.stdout.flush() 
-    tmp_dicom = pydicom.read_file(each_train_dicom)
-    tmp_metadata = pd.DataFrame({"patientId": [each_train_dicom.split("/")[-1].split(".")[0]],
-                                 "sex":  [tmp_dicom.PatientSex],
-                                 "view": [tmp_dicom.ViewPosition],
-                                 "age":  [tmp_dicom.PatientAge]})
-    train_metadata = train_metadata.append(tmp_metadata) 
+# train_metadata = pd.DataFrame() 
+# for i, each_train_dicom in enumerate(list_of_train_dicoms): 
+#     sys.stdout.write("Getting metadata: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
+#     sys.stdout.flush() 
+#     tmp_dicom = pydicom.read_file(each_train_dicom)
+#     tmp_metadata = pd.DataFrame({"patientId": [each_train_dicom.split("/")[-1].split(".")[0]],
+#                                  "sex":  [tmp_dicom.PatientSex],
+#                                  "view": [tmp_dicom.ViewPosition],
+#                                  "age":  [tmp_dicom.PatientAge]})
+#     train_metadata = train_metadata.append(tmp_metadata) 
 
-train_metadata.to_csv(os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_INFO_DIR"], "train_metadata.csv"), index=False)
+# train_metadata.to_csv(os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_INFO_DIR"], "train_metadata.csv"), index=False)
 
 
 test_metadata = pd.DataFrame() 
--- src/etl/3_ConvertDICOM2PNG.py
+++ src/etl/3_ConvertDICOM2PNG.py
@@ -17,26 +17,26 @@ with open(os.path.join(WDIR, "../../SETTINGS.json")) as f:
 # SCRIPT #
 ##########
 
-train_dicoms_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TRAIN_DICOMS_DIR"])
+# train_dicoms_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TRAIN_DICOMS_DIR"])
 test_dicoms_dir  = os.path.join(WDIR, "../../", SETTINGS_JSON["RAW_TEST_DICOMS_DIR"])
 
-list_of_train_dicoms = glob.glob(os.path.join(train_dicoms_dir, "*"))
+# list_of_train_dicoms = glob.glob(os.path.join(train_dicoms_dir, "*"))
 list_of_test_dicoms  = glob.glob(os.path.join(test_dicoms_dir, "*"))
 
-train_images_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_IMAGES_CLEAN_DIR"], "orig")
+# train_images_dir = os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_IMAGES_CLEAN_DIR"], "orig")
 test_images_dir  = os.path.join(WDIR, "../../", SETTINGS_JSON["TEST_IMAGES_CLEAN_DIR"], "orig")
 
-if not os.path.exists(train_images_dir): os.makedirs(train_images_dir)
+# if not os.path.exists(train_images_dir): os.makedirs(train_images_dir)
 
 if not os.path.exists(test_images_dir):  os.makedirs(test_images_dir) 
 
-for i, each_train_dicom in enumerate(list_of_train_dicoms): 
-    sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
-    sys.stdout.flush() 
-    tmp_dicom_array = pydicom.read_file(each_train_dicom).pixel_array
-    assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
-    scipy.misc.imsave(os.path.join(train_images_dir, each_train_dicom.split("/")[-1].replace("dcm", "png")),
-                      tmp_dicom_array)
+# for i, each_train_dicom in enumerate(list_of_train_dicoms): 
+    # sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
+    # sys.stdout.flush() 
+    # tmp_dicom_array = pydicom.read_file(each_train_dicom).pixel_array
+    # assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
+    # scipy.misc.imsave(os.path.join(train_images_dir, each_train_dicom.split("/")[-1].replace("dcm", "png")),
+    #                   tmp_dicom_array)
 
 
 for i, each_test_dicom in enumerate(list_of_test_dicoms): 
